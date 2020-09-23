b5006db496ff2cfaf438bb84d21c30269a5fc30d
DihHenriq <diegohenriqsilva@hotmail.com>
2020-09-22 22:02:41 -0300

    PRova
    Atualização

---
+++ Assets/Scripts/ControleJogador.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ControleJogador : MonoBehaviour {
+public Rigidbody jogador;
+public GameObject cenario;
+public float velocidadeJogador;
+public float velocidadeCenario;
+void Start() {
+}
+// Update is called once per frame
+void Update() {
+    transform.Rotate(new Vector3(0,90,0) * Time.deltaTime);
+float direcao = Input.GetAxis("Horizontal");
+//x->lateral, y->altura, z->profundidade
+Vector3 move = new Vector3(direcao*velocidadeJogador,0,0);
+jogador.AddForce(move);
+//move o chao
+cenario.transform.Translate(0,0,velocidadeCenario * Time.deltaTime * -1);
+}
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.CompareTag("Moeda")){
+            Destroy(col.gameObject);
+        } 
+        if (col.gameObject.CompareTag("Obstaculo")){
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+        
+    }
+}
